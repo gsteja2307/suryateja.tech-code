276dd7efd5bf7072387356ef8054aa9325ec6681
gsteja2307 <gsuryateja05@gmail.com>
2021-03-18 13:47:26 +0530

    remove unusedvariables

--- src/views/examples/Works.jsx
+++ src/views/examples/Works.jsx
@@ -8,31 +8,12 @@ import {
   Container,
   Row,
   Col,
-  UncontrolledCarousel
 } from "reactstrap";
 
 // core components
 
 import Footer from "components/Footer/Footer.jsx";
 import IndexNavbar from "components/Navbars/IndexNavbar.jsx"
-
-const carouselItems = [
-  {
-    src: require("assets/img/denys.jpg"),
-    altText: "Slide 1",
-    caption: "Big City Life, United States"
-  },
-  {
-    src: require("assets/img/fabien-bazanegue.jpg"),
-    altText: "Slide 2",
-    caption: "Somewhere Beyond, United States"
-  },
-  {
-    src: require("assets/img/mark-finn.jpg"),
-    altText: "Slide 3",
-    caption: "Stocks, United States"
-  }
-];
 
 let ps = {};
 
